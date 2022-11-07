2e09e9443f4993e21f4e69b2a3cb925449bcaaec
baramubarok <bara.mubarok9110@gmail.com>
2022-11-07 22:40:04 +0700

    Setup Master

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = (env, argv) => {
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
+    },
+    mode: "production",
+
+    devServer: {
+      client: {
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader",
+            },
+            {
+              loader: "css-loader",
+            },
+          ],
+        },
+
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env"],
+              },
+            },
+          ],
+        },
+
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "images",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      /* HTML Webpack Plugin */
+      new HtmlWebpackPlugin({
+        template: "./src/main.html",
+        filename: "index.html",
+      }),
+      new Dotenv({
+        path: `./.env`,
+      }),
+    ],
+  };
+};
